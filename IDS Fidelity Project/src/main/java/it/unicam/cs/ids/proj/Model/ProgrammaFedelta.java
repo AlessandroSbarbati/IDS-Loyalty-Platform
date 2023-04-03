f2f8693f6e304a0afcfab9e5d9f67ab196f00998
AlessandroBenedetti97 <125594577+AlessandroBenedetti97@users.noreply.github.com>
2023-04-03 17:20:15 +0200

    Add files via upload

---
+++ IDS Fidelity Project/src/main/java/it/unicam/cs/ids/proj/Model/ProgrammaFedelta.java
@@ -0,0 +1,11 @@
+package it.unicam.cs.ids.proj.Model;
+
+public class ProgrammaFedelta {
+
+    double spesa;
+
+    double getSpesa() {return spesa;}
+}
+
+
+
---
+++ IDS Fidelity Project/src/main/java/it/unicam/cs/ids/proj/Model/PuntoVendita.java
@@ -0,0 +1,29 @@
+package it.unicam.cs.ids.proj.Model;
+
+public class PuntoVendita {
+
+    String nome;
+    Integer partitaIVA;
+    String indirizzo;
+    int codiceAttivita;
+    ProgrammaFedelta pf;
+
+    public PuntoVendita(String nome, String indirizzo, Integer partitaIVA){
+     this.nome=nome;
+     this.indirizzo = indirizzo;
+     this.partitaIVA= partitaIVA;
+
+    }
+
+    public int getCodiceAttivita() {
+        return codiceAttivita;
+    }
+
+    public void setCodiceAttivita(int codiceAttivita) {
+        this.codiceAttivita = codiceAttivita;
+    }
+
+    void setProgrammaFedelta(ProgrammaFedelta pf) {
+        this.pf= pf;
+    }
+}
