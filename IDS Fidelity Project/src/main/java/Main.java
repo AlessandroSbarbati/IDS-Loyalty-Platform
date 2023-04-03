3a5e1fdcdd2e3c790263bb3c99d05297abeeab1a
Waleedkhalid98 <129294006+Waleedkhalid98@users.noreply.github.com>
2023-04-03 18:22:54 +0200

    Delete IDS Fidelity Project directory

--- IDS Fidelity Project/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import View.AutenticazioneView;
-
-
-public class Main {
-    public static void main(String[] args) {
-        AutenticazioneView init = new AutenticazioneView();
-        init.accesso();
-    }
-}
